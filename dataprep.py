cbb7f4fa3e8f2e7702130254c2588543cea35195
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 00:26:25 +0000

    Adding numeric_owner as keyword arguement

--- dataprep.py
+++ dataprep.py
@@ -98,7 +98,7 @@ def full_extract(args, fname):
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(tar, args.save_path)
